a982047bf4dd0f426eaef20e2629688f8e50141f
Jacob Hotz <lordgaming1010@gmail.com>
2024-03-05 13:52:49 -0800

    ...

---
+++ playground.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('playground', {
+    /**
+     * Code within this function will be called when everything in
+     * <a-scene> is ready and loaded.
+     */
+
+    init: function () {
+        console.log('Welcome, class!');
+    },
+});
