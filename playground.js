148c0c8c2fc4cdee2649e4d61be7a71646636387
Jacob Hotz <lordgaming1010@gmail.com>
2024-03-19 13:47:58 -0700

    finish lesson 8

--- playground.js
+++ playground.js
@@ -6,5 +6,33 @@ AFRAME.registerComponent('playground', {
 
     init: function () {
         console.log('Welcome, class!');
+
+        let sceneEl = document.querySelector('a-scene');
+        console.log(sceneEl);
+        let boxEl = sceneEl.querySelector('a-box');
+        boxEl.setAttribute('rotation', {x:0, y:45, z:45});
+
+        for (let i = 0; i < 99; i++) {
+            let newBox = document.createElement('a-box');
+            newBox.setAttribute('position', getRandomPosition());
+            newBox.addEventListener('foo', function() {
+                newBox.setAttribute('color', 'red');
+            });
+            newBox.emit('foo');
+            sceneEl.appendChild(newBox);
+            
+        }
+
+        console.log(boxEl);
+
     },
 });
+
+function getRandomPosition() {
+    return {
+        x: (Math.random() - 0.5) * 10,
+        y: (Math.random() - 0.5) * 10,
+        z: (Math.random() - 0.5) * 10
+    };
+}
+
